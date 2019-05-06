9c40d46b9829746d06d466f29e9eba46ad6c101b
babkevo <njugunamwaura@gmail.com>
2019-05-06 18:59:47 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ let answer = require("./answer.model");
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb+srv://admin:rudolf82@cluster0-hch4d.mongodb.net/test?retryWrites=true", {
+mongoose.connect("mongodb+srv://admin:<password>@cluster0-hch4d.mongodb.net/test?retryWrites=true", {
   useNewUrlParser: true
 });
 const connection = mongoose.connection;
